2883f426f5f464b0cdcc73b8176b7bfb3c022ce6
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-12-08 11:03:59 -0500

    strongly type `_app.tsx`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import Favicon, { DefaultMeta } from "~components/meta";
 
 import "../styles/globals.css";
 
-function App({ Component, pageProps }) {
+function App({ Component, pageProps }:AppProps) {
   return (<div>
     <Favicon />
     <Component {...pageProps} />
