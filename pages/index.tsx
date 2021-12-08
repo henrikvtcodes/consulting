4c5928ddd65f9786114d147c942a33b49c48306b
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-12-07 22:59:31 -0500

    better landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,28 +6,32 @@ import logo from "~pub/henriklogo.png";
 
 const Home:NextPage = () => {
   return (
-    <main className="text-black bg-gray-100 grid justify-center place-content-center  w-screen h-screen">
-      {/* <Image src={"/henriklogo.png"} className='w-24 h-24' width={96} height={96} alt="henrik's logo"/> */}
-      <b className="text-4xl mx-2 justify-self-center ">
-        {" "}
-        Henrik Van Tassell Consulting{" "}
-      </b>
-      <i className="justify-self-center">
-        {" "}
-        ~Technology services for residential and small business~{" "}
-      </i>
-      <br />
-      <p className="max-w-3xl">
-        {" "}
-        This website is coming soon (I promise it&apos;ll be pretty cool!). In
-        the meantime, I&apos;m still open to take clients, so you can contact me
-        at the information below.{" "}
-      </p>
-      <br />
-      <div>
-        {" "}
-        <b>Email me at:</b>{" "}
-        <a href="mailto:consulting@henrikvt.com">consulting@henrikvt.com</a>{" "}
+    <main className="text-black bg-gray-100 w-screen h-screen my-auto">
+      <div className="grid justify-center place-content-center mx-2 my-4">
+        <div className='justify-self-center'>
+          <Image src={"/henriklogo.png"} className='w-24 h-24 justify-self-center' width={96} height={96} alt="henrik's logo"/>
+        </div>
+        <b className="text-4xl mx-2 justify-self-center ">
+          {" "}
+          Henrik Van Tassell Consulting{" "}
+        </b>
+        <i className="justify-self-center">
+          {" "}
+          ~Technology services for residential and small business~{" "}
+        </i>
+        <br />
+        <p className="max-w-3xl">
+          {" "}
+          This website is coming soon (I promise it&apos;ll be pretty cool!). In
+          the meantime, I&apos;m still open to take clients, so you can contact me
+          at the information below.{" "}
+        </p>
+        <br />
+        <div>
+          {" "}
+          <b>Email me at:</b>{" "}
+          <a href="mailto:consulting@henrikvt.com">consulting@henrikvt.com</a>{" "}
+        </div>
       </div>
     </main>
   );
