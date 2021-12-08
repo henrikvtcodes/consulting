4b74103d38e5c51541c5d51b28d95f74fe143d50
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-12-08 13:56:26 -0500

    create supabase client with service role key

--- utils/supabase/index.ts
+++ utils/supabase/index.ts
@@ -1,3 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string);
+
+export const supabaseServer = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.SUPABASE_SERVICE_ROLE_KEY as string)
