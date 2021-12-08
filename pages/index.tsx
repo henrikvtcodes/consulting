f6197ffdff1ee5fe421e9ff7060d34d92a90c1e0
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-12-07 22:49:52 -0500

    basic landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,35 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
+import logo from "~pub/henriklogo.png";
 
 const Home:NextPage = () => {
   return (
-    <div className="">
-    </div>
-  )
+    <main className="text-black bg-gray-100 grid justify-center place-content-center  w-screen h-screen">
+      {/* <Image src={"/henriklogo.png"} className='w-24 h-24' width={96} height={96} alt="henrik's logo"/> */}
+      <b className="text-4xl mx-2 justify-self-center ">
+        {" "}
+        Henrik Van Tassell Consulting{" "}
+      </b>
+      <i className="justify-self-center">
+        {" "}
+        ~Technology services for residential and small business~{" "}
+      </i>
+      <br />
+      <p className="max-w-3xl">
+        {" "}
+        This website is coming soon (I promise it&apos;ll be pretty cool!). In
+        the meantime, I&apos;m still open to take clients, so you can contact me
+        at the information below.{" "}
+      </p>
+      <br />
+      <div>
+        {" "}
+        <b>Email me at:</b>{" "}
+        <a href="mailto:consulting@henrikvt.com">consulting@henrikvt.com</a>{" "}
+      </div>
+    </main>
+  );
 }
 
 export default Home
