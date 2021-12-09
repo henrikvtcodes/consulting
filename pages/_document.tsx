95f6fc2892aaede5f213aa75709842836e6ec42e
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-12-09 11:24:45 -0500

    comment out analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,12 +10,12 @@ class AppDocument extends Document {
     return (
       <Html className="h-full bg-gray-50">
         <Head>
-          <script
+          {/* <script
             async
             defer
             data-website-id="8157dbf3-8f86-4656-a8ae-d3010628e50d"
             src="https://analytics.henriktech.com/umami.js"
-          />
+          /> */}
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         </Head>
         <body className="h-full">
