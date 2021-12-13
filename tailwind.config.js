8ae500b8efd78d29d706c75ec417ccf08c791891
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-12-13 10:50:44 -0500

    better updated config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,47 +2,26 @@ const colors = require("tailwindcss/colors");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  mode: "jit",
-  purge: [
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./layouts/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        transparent: "transparent",
-        current: "currentColor",
-        black: colors.black,
-        white: colors.white,
-        indigo: colors.indigo,
-        red: colors.rose,
-        fuschia: colors.fuschia,
-        gray: {
-          DEFAULT: colors.gray,
-          true: colors.trueGray,
-          cool: colors.coolGray,
-          blue: colors.blueGray,
+        customGray: {
           manatee: "#9CA3AF",
           jet: "#2c2f33",
           charleston: "#23272a",
         },
-        yellow: {
-          DEFAULT: colors.yellow,
-          amber: colors.amber,
-        },
-        orange: {
-          DEFAULT: colors.orange,
+        customOrange: {
           spanish: "#e36414",
           dark: "#fb8b24",
           flame: "#e25822",
         },
-        blue: {
-          DEFAULT: colors.blue,
-          sky: colors.sky,
-          cyan: colors.cyan,
-          teal: colors.teal,
+        customBlue: {
           celadon: "#457b9d",
           prussian: "#1d3557",
           pacific: "#0eb1d2",
@@ -50,9 +29,7 @@ module.exports = {
           jeans: "#00a6fb",
           twitter: "#1DA1F2",
         },
-        purple: {
-          DEFAULT: colors.purple,
-          violet: colors.violet,
+        customPurple: {
           amethyst: "#a663cc",
           wisteria: "#b298dc",
           electric: "#b14aed",
@@ -72,7 +49,6 @@ module.exports = {
   },
   plugins: [
     require("@tailwindcss/typography"),
-    require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/line-clamp"),
     require("@tailwindcss/forms"),
   ],
