a1958d768d814cf29f4a7977c644bc66d9f31099
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-12-14 11:10:44 -0500

    create solutions pages

---
+++ pages/solutions/Networking.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+import HomeLayout from 'layouts/home'
+import { DefaultMeta } from '~components/meta'
+
+const Page:NextPage = (props) => {
+  return (
+    <HomeLayout>
+      
+    </HomeLayout>
+  );
+}
+
+export default Page;
