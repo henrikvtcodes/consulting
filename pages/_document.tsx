b0a2dc2933b5b6915651d49ce70f071256d07d3a
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-12-14 11:05:50 -0500

    change analytics behavior add turnary operator

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,16 +8,25 @@ class AppDocument extends Document {
 
   render() {
     return (
-      <Html className="h-full bg-gray-50">
+      <Html className="h-full">
         <Head>
-          { process.env.NODE_ENV === "production" &&
+          {process.env.NODE_ENV === "production" ? (
             <script
-            async
-            defer
-            data-website-id="8157dbf3-8f86-4656-a8ae-d3010628e50d"
-            src="/umami.js"
-            data-host-url="http://analytics.henriktech.com"
-          /> }
+              async
+              defer
+              data-website-id="8157dbf3-8f86-4656-a8ae-d3010628e50d"
+              src="/umami.js"
+              data-host-url="http://analytics.henriktech.com"
+            />
+          ) : (
+            <script
+              async
+              defer
+              data-website-id="77e5daa9-9e45-4a71-9e24-018a2acdac45"
+              src="/umami.js"
+              data-host-url="http://analytics.henriktech.com"
+            />
+          )}
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         </Head>
         <body className="h-full">
