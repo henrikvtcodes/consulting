61eb86d5f7b47333e714cb02e3f15f1db3142371
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-12-25 23:44:42 -0500

    remove xstate  | i cant fucking decide  | adasdmlaksmdalsdjand

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,12 @@
 import type { AppProps } from 'next/app'
 import Favicon from "~components/meta";
-import { AuthProvider } from '~utils/xstate/providers';
-
 import "../styles/globals.css";
 
 function App({ Component, pageProps }:AppProps) {
   return (
   <div>
-    <AuthProvider>
       <Favicon />
       <Component {...pageProps} />
-    </AuthProvider>
   </div>
   );
 }
--- utils/xstate/index.ts
+++
@@ -1,1 +0,0 @@
-import { createMachine } from "xstate";
