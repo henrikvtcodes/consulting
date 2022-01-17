1283d34a34561eec38cfb7e014588ed7a13d00d6
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-01-17 09:34:14 -0500

    i mean yeah just renamed

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -1,34 +1,28 @@
 import { useRouter } from "next/router";
 import Typical from "react-typical";
 import { useEffect } from "react";
+import { useSession } from "next-auth/react";
 
-import { supabase } from "~utils/Supabase";
-
-const Page = () => { // Page that directs user to administrator or client dashboard if logged in, directs to home page if not logged in
+const Page = () => {
+  // Page that directs user to administrator or client dashboard if logged in, directs to home page if not logged in
   const router = useRouter();
+  const { data: session, status } = useSession();
 
   useEffect(() => {
-    const user = supabase.auth.user();
-
-    if (!user) {
+    if (status === "unauthenticated") {
       console.log("User not logged in");
       setTimeout(() => {
-        router.push("/");
+        router.push("/auth/sign-in");
       }, 10000);
     }
-  })
-  
+  });
+
   return (
     <main className="h-screen w-screen bg-gray-50 grid place-content-center place-items-center">
       <h1 className="text-4xl text-brand-text1">
-        <Typical
-          steps={[
-            "Please wait while we log you in..."
-          ]}
-          loop={1}
-        />
+        <Typical steps={["Please wait while we log you in..."]} loop={1} />
       </h1>
     </main>
   );
-}
+};
 export default Page;
