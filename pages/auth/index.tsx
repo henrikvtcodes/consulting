a792ccdeb1f8cb5f6a4759d1a083c2c52f923f42
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-01-30 23:13:51 -0500

    pwease daddy typescript

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -1,8 +1,13 @@
 import { useRouter } from "next/router";
+// @ts-ignore
 import Typical from "react-typical";
 import { useEffect } from "react";
 import { useSession } from "next-auth/react";
 
+import FormPageLayout from "layouts/formPage";
+import { getUserRole } from "~utils/lookupUser.server";
+
+// eslint-disable-next-line
 const Page = () => {
   // Page that directs user to administrator or client dashboard if logged in, directs to home page if not logged in
   const router = useRouter();
@@ -15,14 +20,14 @@ const Page = () => {
         router.push("/auth/sign-in");
       }, 10000);
     }
-  });
+  }, [router, status, session]);
 
   return (
-    <main className="h-screen w-screen bg-gray-50 grid place-content-center place-items-center">
+    <FormPageLayout>
       <h1 className="text-4xl text-brand-text1">
         <Typical steps={["Please wait while we log you in..."]} loop={1} />
       </h1>
-    </main>
+    </FormPageLayout>
   );
 };
 export default Page;
