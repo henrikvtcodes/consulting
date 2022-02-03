c06bb23842b652f7d379bdededcc6765b66b55a9
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-02-02 22:32:54 -0500

    update header links

--- components/tui/headerM.tsx
+++ components/tui/headerM.tsx
@@ -58,12 +58,12 @@ const HeaderM = () => {
           {/* NOTE Add Flyout menu for more options here */}
         </Popover.Group>
         <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-          <NextLink href={"sign-in"} prefetch={false} passHref>
+          <NextLink href={"/auth/sign-in"} prefetch={false} passHref>
             <a className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
               Sign in
             </a>
           </NextLink>
-          <NextLink href={"sign-in"} prefetch={false} passHref>
+          <NextLink href={"/auth/sign-in"} prefetch={false} passHref>
             <a className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-brand-primary hover:bg-brand-accent1h">
               Sign up
             </a>
