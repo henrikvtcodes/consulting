c1d08ab522217776db071ae628ff73af27e5b941
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-03-01 14:01:24 -0500

    change analytics behavior

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,23 +10,13 @@ class AppDocument extends Document {
     return (
       <Html className="h-full">
         <Head>
-          {process.env.NODE_ENV === "production" ? (
-            <script
-              async
-              defer
-              data-website-id="8157dbf3-8f86-4656-a8ae-d3010628e50d"
-              src="/umami.js"
-              data-host-url="https://analytics.henriktech.com"
-            />
-          ) : (
-            <script
-              async
-              defer
-              data-website-id="77e5daa9-9e45-4a71-9e24-018a2acdac45"
-              src="/umami.js"
-              data-host-url="https://analytics.henriktech.com"
-            />
-          )}
+          <script
+            async
+            defer
+            data-website-id="77e5daa9-9e45-4a71-9e24-018a2acdac45"
+            src="/umami.js"
+            data-host-url="https://analytics.henriktech.com"
+          />
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         </Head>
         <body className="h-full">
