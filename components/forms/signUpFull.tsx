8c12bc9c294d68ba34b41db9a508e5a9dbbb9556
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-03-02 22:46:18 -0500

    visually complete sign up form

--- components/forms/signUpFull.tsx
+++ components/forms/signUpFull.tsx
@@ -1,49 +1,118 @@
-import { useForm, FormProvider } from "react-hook-form";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import PasswordStrengthBar from "react-password-strength-bar";
+import {
+  ExclamationIcon,
+  CheckIcon,
+  RefreshIcon,
+} from "@heroicons/react/outline";
 
 import formStyles from "~styles/forms.module.css";
-import { testOnSubmit } from "~utils/signin";
-import * as SignUpParts from "./signUpParts"
+import { useDebouncedFunc } from "~utils/hooks/useDebouncedReq";
+import {
+  validateCode,
+  submitCode,
+  SubmitCodeReturnType,
+} from "~utils/submitCode";
 
-export type SignUpFormProps = {
-  firstname: string;
-  lastname: string;
-  phone: string;
-  email: string;
-  password: string;
-  confirmPassword: string;
-  refCode: string;
-};
-
-type Step = {
-  index: number;
-}
+const useValidateInviteCode = () =>
+  useDebouncedFunc<SubmitCodeReturnType>(
+    (code: string) => validateCode(code),
+    1000
+  );
 
 const SignUpForm = () => {
+  // Custom hook to prevent excesseive requests
+  const { inputText, setInputText, results: result } = useValidateInviteCode();
 
-  const formMethods = useForm<SignUpFormProps>();
-  const {
-    register,
-    handleSubmit,
-    watch,
-    setError,
-    formState: { errors, isDirty },
-  } = formMethods;
+  const safeToSubmit =
+    result.status === "success" &&
+    result.result &&
+    result.result?.isValid === true;
 
-  const [ step, setStep ] = useState<Step>({ index: 0 });
-
-  const next = () => setStep(() => ({ index: step.index++ }));
+  const color =
+    result.status === "success" && safeToSubmit
+      ? "emerald"
+      : result.status === "loading"
+      ? "yellow"
+      : "red";
 
   return (
-    <FormProvider {...formMethods}>
-      <form onSubmit={testOnSubmit}>
-        {
-          
-        }
+    <div className="md:w-[464px] flex flex-col">
+      <form
+        className="flex flex-col mx-2 md:mx-16 space-y-3"
+        action="/api/invitecode"
+        method="post"
+      >
+        <div className={formStyles.Input}>
+          <label htmlFor={"code"}>Invite Code</label>
+          <input
+            id={"code"}
+            className="w-2/3"
+            placeholder={"Enter Invite Code"}
+            type={"text"}
+            autoComplete={"off"}
+            maxLength={6}
+            minLength={6}
+            pattern={"[A-Fa-f0-9]{6}"}
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            required
+          />
+        </div>
+
+        {inputText && (
+          <div
+            className={`w-full mt-0 px-1 py-2 h-16 bg-${color}-100 rounded text-sm flex grow justify-left align-middle`}
+          >
+            {result.status === "loading" ? (
+              <div className="w-full flex justify-center align-middle items-center ">
+                <div className="flex-shrink-0" dir="rtl">
+                  <RefreshIcon
+                    className="h-7 w-7 text-yellow-400 animate-spin"
+                    aria-hidden="true"
+                  />
+                </div>{" "}
+              </div>
+            ) : (
+              <div className="w-full flex justify-left items-top text-sm">
+                <div className="flex-shrink-0">
+                  {safeToSubmit ? (
+                    <CheckIcon
+                      className="h-5 w-5 text-emerald-400 mx-1"
+                      aria-hidden="true"
+                    />
+                  ) : (
+                    <ExclamationIcon
+                      className="h-5 w-5 text-red-400 mx-1"
+                      aria-hidden="true"
+                    />
+                  )}
+                </div>
+                <div className="">
+                  <h3 className={`text-sm font-semibold text-${color}-800`}>
+                    {safeToSubmit ? "Valid" : "Invalid"}
+                  </h3>
+                  {result.result ? (
+                    <div
+                      className={`mt-1 text-sm font-medium text-${color}-700`}
+                    >
+                      <p>{result.result.message}</p>
+                    </div>
+                  ) : (
+                    <div
+                      className={`mt-1 text-sm font-medium text-${color}-700`}
+                    >
+                      <p>{"Code was not found"}</p>
+                    </div>
+                  )}
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+        <button className={formStyles.Button} disabled={!safeToSubmit}>
+          Submit
+        </button>
       </form>
-    </FormProvider>
+    </div>
   );
 };
 
--- pages/auth/sign-up.tsx
+++ pages/auth/sign-up.tsx
@@ -1,120 +1,23 @@
 import { NextPage } from "next";
-import {
-  ExclamationIcon,
-  CheckIcon,
-  RefreshIcon,
-} from "@heroicons/react/outline";
 
 import FormPageLayout from "layouts/formPage";
 import FormWindow from "~components/forms/window";
 import SignUpForm from "~components/forms/signUpFull";
-import formStyles from "~styles/forms.module.css";
 import { useDebouncedFunc } from "~utils/hooks/useDebouncedReq";
-import { submitCode, SubmitCodeReturnType } from "~utils/submitCode";
-
-const useValidateInviteCode = () =>
-  useDebouncedFunc<SubmitCodeReturnType>(
-    (code: string) => submitCode(code),
-    1000
-  );
+import { validateCode, SubmitCodeReturnType } from "~utils/submitCode";
 
 // eslint-disable-next-line
 const Page: NextPage = (props) => {
-  const { inputText, setInputText, results: result } = useValidateInviteCode();
-
-  const safeToSubmit =
-    result.status === "success" &&
-    result.result &&
-    result.result?.isValid === true;
-  console.log("Res: Code isValid: ", result.result);
-
-  const color =
-    result.status === "success" && safeToSubmit
-      ? "emerald"
-      : result.status === "loading"
-      ? "yellow"
-      : "red";
-
   return (
     <FormPageLayout title="Sign Up" desc="Confirm your account">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="my-6 text-center text-3xl font-extrabold text-gray-900">
+        <h2 className="my-2 md:my-6 text-center text-3xl font-extrabold text-gray-900">
           Sign Up
         </h2>
       </div>
 
       <FormWindow>
-        <div className="md:w-[464px] flex flex-col">
-          <form className="flex flex-col mx-16 space-y-3">
-            <div className={formStyles.Input}>
-              <label htmlFor={"code"}>Invite Code</label>
-              <input
-                id={"code"}
-                className="w-2/3"
-                placeholder={"Enter Invite Code"}
-                type={"text"}
-                autoComplete={"off"}
-                value={inputText}
-                onChange={(e) => setInputText(e.target.value)}
-                required
-              />
-            </div>
-
-            {inputText && (
-              <div
-                className={`w-full mt-0 px-1 py-2 h-16 bg-${color}-100 rounded text-sm flex grow justify-left align-middle`}
-              >
-                {result.status === "loading" ? (
-                  <div className="w-full flex justify-center align-middle items-center ">
-                    <div className="flex-shrink-0">
-                      <RefreshIcon
-                        className="h-7 w-7 text-yellow-400 animate-spin"
-                        aria-hidden="true"
-                      />
-                    </div>{" "}
-                  </div>
-                ) : (
-                  <div className="w-full flex justify-left items-top text-sm">
-                    <div className="flex-shrink-0">
-                      {safeToSubmit ? (
-                        <CheckIcon
-                          className="h-5 w-5 text-emerald-400 mx-1"
-                          aria-hidden="true"
-                        />
-                      ) : (
-                        <ExclamationIcon
-                          className="h-5 w-5 text-red-400 mx-1"
-                          aria-hidden="true"
-                        />
-                      )}
-                    </div>
-                    <div className="">
-                      <h3 className={`text-sm font-semibold text-${color}-800`}>
-                        {safeToSubmit ? "Valid" : "Invalid"}
-                      </h3>
-                      {result.result ? (
-                        <div
-                          className={`mt-1 text-sm font-medium text-${color}-700`}
-                        >
-                          <p>{result.result.message}</p>
-                        </div>
-                      ) : (
-                        <div
-                          className={`mt-1 text-sm font-medium text-${color}-700`}
-                        >
-                          <p>{"Code was not found"}</p>
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                )}
-              </div>
-            )}
-            <button className={formStyles.Button} disabled={!safeToSubmit}>
-              Submit
-            </button>
-          </form>
-        </div>
+        <SignUpForm />
       </FormWindow>
     </FormPageLayout>
   );
