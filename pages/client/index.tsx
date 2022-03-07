14f91b711990ba11065d79fa2f8195cbd3d040c4
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-03-07 14:37:51 -0500

    client pages

--- pages/client/index.tsx
+++ pages/client/index.tsx
@@ -1,13 +1,22 @@
 import type { NextPage } from "next";
+import { useSession } from "next-auth/react";
 
 import { RoleLayout } from "layouts/RoleLayout";
 import { ClientDashLayout } from "layouts/ClientDash";
 
 const ClientHome: NextPage = (props) => {
+  const session = useSession();
+
   return (
     <ClientDashLayout>
       <main>
-        <h1 className="text-4xl">Client Home</h1>
+        <h1 className="text-4xl">
+          Hi,{" "}
+          <span className="font-semibold">
+            {session.data?.user?.name ? session.data?.user?.name : "User"}
+          </span>
+          !
+        </h1>
       </main>
     </ClientDashLayout>
   );
