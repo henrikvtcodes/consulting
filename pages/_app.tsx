8bd85633b4bc61021821b073cf3c02fd50a9be26
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-03-08 14:07:44 -0500

    add redux toolkit & react-redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import type { AppProps } from "next/app";
+import type { AppInitialProps, AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import useSWR, { SWRConfig } from "swr";
 import axios from "axios";
@@ -7,20 +7,25 @@ import Favicon from "~components/meta";
 import { WarningBanner } from "~components/tui/warning-banner";
 import "../styles/globals.css";
 import { localStorageCache } from "~utils/swr_localstoragecache";
+import { store } from "state/store";
+import { Provider } from "react-redux";
 
+// prettier-ignore
 function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SWRConfig
-      value={{
-        fetcher: (url: string) => axios.get(url).then((res) => res.data),
-        // provider: localStorageCache,
-      }}
-    >
-      <SessionProvider session={session}>
-        {process.env.NEXT_PUBLIC_PROD_WARN === "true" && <WarningBanner />}
-        <Component {...pageProps} />
-      </SessionProvider>
-    </SWRConfig>
+    <Provider store={store}>
+      <SWRConfig
+        value={{
+          fetcher: (url: string) => axios.get(url).then((res) => res.data),
+          provider: () => new Map(),
+        }}
+      >
+        <SessionProvider session={session}>
+          {process.env.NEXT_PUBLIC_PROD_WARN === "true" && <WarningBanner />}
+          <Component {...pageProps} />
+        </SessionProvider>
+      </SWRConfig>
+    </Provider>
   );
 }
 
