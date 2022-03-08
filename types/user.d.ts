b998d3af9b392ec3310f04d4eab1d9061dfe1fee
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-03-08 17:06:59 -0500

    new address fields

--- types/user.d.ts
+++ types/user.d.ts
@@ -5,12 +5,25 @@ export interface UserMetadata {
     role: string;
     phone?: string;
     image: string;
+    address_line1?: string;
+    address_line2?: string;
+    city?: string;
+    state?: string;
+    postal_code?: string;
 }
 
 export interface UserMetaData_Alter {
     name?: string;
     phone?: string;
     photo_url?: string;
+    address_line1?: string;
+    address_line2?: string;
+    city?: string;
+    state?: string;
+    postal_code?: string;
+}
+
+export type Address = {
     street_address?: string;
     city?: string;
     state?: string;
