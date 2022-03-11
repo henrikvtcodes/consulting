53a5cd0a98813383e89108ebe2544ff25d690eef
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-03-11 12:09:50 -0500

    change prisma global

--- global.d.ts
+++ global.d.ts
@@ -4,6 +4,8 @@ declare module 'react-typical';
 
 import type { PrismaClient as PrismaClientType } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClientType | undefined; // I'm sorry, but `var` is the best I can do.
+interface CustomNodeJsGlobal extends NodeJS.Global {
+  prisma: PrismaClientType
 }
+
+declare const global: CustomNodeJsGlobal
