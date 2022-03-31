5e5337e55a06669005adf3a3376aee6df6424329
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-03-31 09:29:06 -0400

    pls

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import { Fragment } from "react";
 
 import Favicon from "~components/meta";
 import { WarningBanner } from "~components/tui/warning-banner";
@@ -6,10 +7,10 @@ import "../styles/globals.css";
 import { Provider } from "react-redux";
 
 // prettier-ignore
-function App({ Component, pageProps: { session, ...pageProps } }:AppProps) {
+function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <div>
-      {process.env.NODE_ENV === "production" && <WarningBanner />}
+      {process.env.NODE_ENV === "production" ? <WarningBanner /> : <Fragment/>}
       <Favicon />
       <Component {...pageProps} />
     </div>
