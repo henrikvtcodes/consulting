e29abf3ca20109ca3b658a91ec67063014c12818
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-04-05 11:59:44 -0400

    bruh

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,8 +7,7 @@ import { WarningBanner } from "~components/tui/warning-banner";
 import "../styles/globals.css";
 import { Provider } from "react-redux";
 
-// prettier-ignore
-function App({ Component, pageProps: { session, ...pageProps } }) {
+function App({ Component, pageProps: { session, ...pageProps } }:AppProps) {
   return (
     <div>
       {process.env.NODE_ENV === "production" ? <WarningBanner /> : <Fragment />}
