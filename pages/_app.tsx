97055caee1a1c400628a6e206e16c3733424b979
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-04-06 14:35:13 -0400

    cut the shit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,14 @@
-import type { AppProps } from 'next/app'
-import { store } from '../utils/redux/index';
-import { Provider } from "react-redux";
-import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
 import Favicon, { DefaultMeta } from "~components/meta";
 
 import "../styles/globals.css";
 
-function App({ Component, pageProps }:AppProps) {
-  return (<div>
-    <Favicon />
-    <Component {...pageProps} />
-  </div>
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      <Favicon />
+      <Component {...pageProps} />
+    </div>
   );
 }
 
--- pages/api/userCookie.ts
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-import { supabase } from '~utils/supabase'
-
-export default async function authHandler( req: NextApiRequest, res: NextApiResponse ) {
-    await supabase.auth.api.setAuthCookie(req, res);
-}
