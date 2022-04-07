5ae7055d888e226a3e0ef7d4a89a26a0fb837a6f
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-04-07 12:03:46 -0400

    custom page view goal thing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,23 @@
 import type { AppProps } from "next/app";
 import Favicon, { DefaultMeta } from "~components/meta";
 import PlausibleProvider from "next-plausible";
+import { useEffect } from "react";
+
+import { usePlausible } from "next-plausible";
 
 import "../styles/globals.css";
 
 function App({ Component, pageProps }: AppProps) {
+  const plausible = usePlausible();
+
+  useEffect(() => {
+    plausible("siteView", {
+      props: {
+        v: "Landing",
+      },
+    });
+  }, [plausible]);
+
   return (
     <div>
       <Favicon />
