b37741a39cce3c9fa4e16e49f5d889c9fd562441
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-04-12 10:24:10 -0400

    remove redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,13 @@
-import type { AppInitialProps, AppProps } from "next/app";
+import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import useSWR, { SWRConfig } from "swr";
+import { SWRConfig } from "swr";
 import axios from "axios";
 import PlausibleProvider, { usePlausible } from "next-plausible";
+import { Fragment, useEffect } from "react";
 
 import Favicon from "~components/meta";
 import { WarningBanner } from "~components/tui/warning-banner";
 import "../styles/globals.css";
-import { localStorageCache } from "~utils/swr_localstoragecache";
-import { Fragment, useEffect } from "react";
-import { store } from "state/store";
-import { Provider } from "react-redux";
 
 // prettier-ignore
 function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
@@ -21,13 +18,13 @@ function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   }, [plausible]);
   
   return (
-    <Provider store={store}>
       <SWRConfig
         value={{
           fetcher: (url: string) => axios.get(url).then((res) => res.data),
           provider: () => new Map(),
         }}
       >
+      <Favicon />
         <SessionProvider session={session}>
           {process.env.NEXT_PUBLIC_PROD_WARN === "true" ? (
             <WarningBanner />
@@ -44,7 +41,6 @@ function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
           </PlausibleProvider>
         </SessionProvider>
       </SWRConfig>
-    </Provider>
   );
 }
 
