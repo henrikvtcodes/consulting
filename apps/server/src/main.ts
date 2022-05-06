97a5e2f8bb7928e6edabb1e6ab53f072d007d206
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-05-06 13:44:56 -0400

    enable cors

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -12,6 +12,14 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://dev.consulting.henrikvt.com',
+      'https://consulting.henrikvt.com',
+    ],
+  });
+
   await app.listen(3333);
 }
 bootstrap();
