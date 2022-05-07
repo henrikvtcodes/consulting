f6ab1659d2536de29b1ad4e9c54b0ef0e83844f4
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-05-07 15:38:10 -0400

    stripe stuff

--- apps/server/src/stripe/stripe.controller.ts
+++ apps/server/src/stripe/stripe.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('stripe')
-export class StripeController {}
+export class StripeController {
+  @Post('webhook')
+  async webhook(@Body() body) {
+    return 'ok';
+  }
+}
--- apps/server/src/stripe/stripe.module.ts
+++ apps/server/src/stripe/stripe.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { StripeModule as StripeHandler } from '@golevelup/nestjs-stripe';
+
 import { StripeController } from './stripe.controller';
+import { StripeService } from './stripe.service';
 
 @Module({
-  controllers: [StripeController]
+  imports: [
+    StripeHandler.forRoot(StripeHandler, {
+      apiKey: process.env.STRIPE_SECRET_KEY,
+      webhookConfig: {
+        stripeWebhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
+      },
+    }),
+  ],
+  controllers: [StripeController],
+  providers: [StripeService],
 })
 export class StripeModule {}
