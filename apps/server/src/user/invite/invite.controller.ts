f373bb40322b2ff4fe0caaa8b006e50387bf6774
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-05-10 08:28:12 -0400

    server: invite validation

---
+++ apps/server/src/user/invite/invite.controller.ts
@@ -0,0 +1,102 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { User } from '@prisma/client';
+import { rmSync } from 'fs';
+import { SessionGuard } from '../../auth/nextauth-session.guard';
+import { Roles, ROLES_KEY } from '../../auth/role.decorator';
+import { Role } from '../../auth/role.enum';
+import { RolesGuard } from '../../auth/role.guard';
+import { InviteService } from './invite.service';
+
+@Controller('invite')
+@UseGuards(SessionGuard, RolesGuard)
+export class InviteController {
+  constructor(private inviteService: InviteService) {}
+
+  @Post() // Allow admin to create a new invite code
+  @Roles(Role.ADMIN)
+  async getNewInvite(@Req() req, @Body() body) {
+    const user = req.user as User;
+
+    const requestCode: string | undefined = body['code'];
+  }
+
+  @Patch() // Allow user to use an invite code
+  async useInvite(@Req() req, @Body() body) {
+    const user = req.user as User;
+
+    const code: string | undefined = body['code'];
+
+    if (!code) {
+      throw new HttpException('No code provided', HttpStatus.BAD_REQUEST);
+    }
+
+    if (user.isInvited && user.role === 'client') {
+      throw new HttpException(
+        'You are not permitted to perform this operation',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    switch (user.role) {
+      case 'admin':
+        const resultAdmin = await this.inviteService.useInviteAdmin(code);
+        if (resultAdmin === null) {
+          throw new HttpException(
+            'A Problem Occurred',
+            HttpStatus.INTERNAL_SERVER_ERROR,
+          );
+        }
+
+      case 'client':
+        const resultClient = await this.inviteService.useInviteClient(
+          user,
+          code,
+        );
+        if (resultClient === null) {
+          throw new HttpException(
+            'A Problem Occurred',
+            HttpStatus.INTERNAL_SERVER_ERROR,
+          );
+        }
+    }
+  }
+
+  @Get('status') // Check if the logged in user is invited
+  async getInviteStatus(@Req() req) {
+    const user = req.user as User;
+    return { invited: user.isInvited };
+  }
+
+  @Get('validate')
+  async validateInvite(@Req() req, @Body() body) {
+    const code = body['code'];
+    const isValid = await this.inviteService.validateInvite(code);
+
+    if (isValid === null) {
+      return {
+        message: 'Code does not exist',
+        isValid: false,
+      };
+    } else if (isValid) {
+      return {
+        message: 'Code is valid',
+        isValid: true,
+      };
+    } else if (isValid === false) {
+      return {
+        message: 'Code is invalid',
+        isValid: false,
+      };
+    }
+  }
+}
---
+++ apps/server/src/user/invite/invite.service.ts
@@ -0,0 +1,107 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { customAlphabet } from 'nanoid';
+
+import { PrismaService } from '../../prisma/prisma.service';
+
+const genHexCode = customAlphabet('abcdefABCDEF123456', 6);
+
+@Injectable()
+export class InviteService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getInvite(code: string) {
+    const invite = await this.prisma.invite.findUnique({
+      where: {
+        token: code,
+      },
+    });
+
+    return invite;
+  }
+
+  async validateInvite(code: string) {
+    const invite = await this.getInvite(code);
+    return !invite.used;
+  }
+
+  async createInvite(code?: string) {
+    let newCode: string;
+
+    if (code) {
+      newCode = code;
+    } else {
+      newCode = genHexCode();
+    }
+
+    const invite = await this.prisma.invite.create({
+      data: {
+        token: newCode,
+      },
+    });
+
+    return invite;
+  }
+
+  async useInviteClient(user: User, code: string) {
+    const invite = await this.getInvite(code);
+
+    if (!invite) {
+      // Invite doesnt exist, return null
+      return null;
+    } else if (invite.used) {
+      // Invite has already been used, return null
+      return null;
+    } else if (user.isInvited) {
+      // User is already invited, return null
+      return null;
+    }
+
+    const updatedInvite = await this.prisma.invite.update({
+      where: {
+        id: invite.id,
+      },
+      data: {
+        used: true,
+        usedBy: user.id,
+        usedOn: new Date(),
+      },
+    });
+
+    await this.prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        isInvited: true,
+      },
+    });
+
+    return updatedInvite;
+  }
+
+  async useInviteAdmin(code: string) {
+    const invite = await this.getInvite(code);
+
+    if (!invite) {
+      // Invite doesnt exist, return null
+      return null;
+    } else if (invite.used) {
+      // Invite has already been used, return null
+      return null;
+    }
+
+    const updatedInvite = await this.prisma.invite.update({
+      where: {
+        id: invite.id,
+      },
+      data: {
+        used: true,
+        usedBy: null,
+        usedOn: new Date(),
+      },
+    });
+
+    return updatedInvite;
+  }
+}
