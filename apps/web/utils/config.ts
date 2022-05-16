ed5494dfd3b01045b82a8f0c9888275efed16184
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-05-16 12:24:32 -0400

    web: api url?

--- apps/web/utils/config.ts
+++ apps/web/utils/config.ts
@@ -17,12 +17,10 @@ import {
 // prettier-ignore-file
 // eslint-disable
 
-// export const API_URL =
-//   process.env.NODE_ENV === "production"
-//     ? process.env.API_BASE_URL
-//     : "http://localhost:3333";
-
-export const API_URL = "http://localhost:3333";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.API_BASE_URL
+    : "http://localhost:3333";
 
 const solutions = [
   {
