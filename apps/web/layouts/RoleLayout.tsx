f507ecec9713675028ca3f42479bd0f7897a83c0
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-05-18 12:23:33 -0400

    web: role layout logic change

--- apps/web/layouts/RoleLayout.tsx
+++ apps/web/layouts/RoleLayout.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 
 import { useRole } from "utils/hooks/useRole";
-import {useUser} from "~utils/hooks/useUser";
+import { useUser } from "~utils/hooks/useUser";
 
 type RoleLayoutProps = {
   roles: string[];
@@ -24,8 +24,8 @@ const RoleLayout = (props: RoleLayoutProps) => {
   const { user } = useUser();
 
   if (user !== undefined) {
-    if (!user?.isInvited) {
-      router.push("/auth/sign-up")
+    if (!user.isInvited) {
+      router.push("/auth/sign-up");
     }
 
     const includes = props.roles.includes(user?.role);
@@ -36,8 +36,6 @@ const RoleLayout = (props: RoleLayoutProps) => {
       router.push("/auth");
     }
   }
-
-
 
   return <>{props.children}</>;
 };
