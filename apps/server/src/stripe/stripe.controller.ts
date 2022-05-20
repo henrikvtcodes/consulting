25ab4454771a99d74ec5c073f11321ad09e22e61
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-05-20 10:32:45 -0400

    server: create stripe billing portal route

--- apps/server/src/stripe/stripe.controller.ts
+++ apps/server/src/stripe/stripe.controller.ts
@@ -1,4 +1,28 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { InjectStripeClient } from '@golevelup/nestjs-stripe';
+import { Body, Controller, Post, Req } from '@nestjs/common';
+import { User as DbUser } from '@prisma/client';
+import Stripe from 'stripe';
+import { CustomerService } from './customer.service';
 
 @Controller('stripe')
-export class StripeController {}
+export class StripeController {
+  constructor(
+    @InjectStripeClient() private stripe: Stripe,
+    private customerService: CustomerService,
+  ) {}
+  @Post('/createPortalSession')
+  async setupCustomer(@Req() req, @Body() body) {
+    const user = req.user as DbUser;
+
+    const customer = await this.customerService.createCustomer(user);
+
+    const session = await this.customerService.createCustomerPortal(
+      customer,
+      body.url,
+    );
+
+    return {
+      url: session.url,
+    };
+  }
+}
