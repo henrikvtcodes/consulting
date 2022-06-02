0c8b35e1811d68e443957262d93234689feb8562
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-06-02 12:17:24 -0400

    server: fix create portal route; add session guard

--- apps/server/src/stripe/stripe.controller.ts
+++ apps/server/src/stripe/stripe.controller.ts
@@ -1,10 +1,12 @@
 import { InjectStripeClient } from '@golevelup/nestjs-stripe';
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { User as DbUser } from '@prisma/client';
 import Stripe from 'stripe';
+import { SessionGuard } from '../auth/nextauth-session.guard';
 import { CustomerService } from './customer.service';
 
 @Controller('stripe')
+@UseGuards(SessionGuard)
 export class StripeController {
   constructor(
     @InjectStripeClient() private stripe: Stripe,
