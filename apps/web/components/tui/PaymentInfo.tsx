20a0e1ec5c646ffa4d18abfab2f00e00aa4264bd
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-06-03 11:54:49 -0400

    web: allow users to edit info on stripe

--- apps/web/components/tui/PaymentInfo.tsx
+++ apps/web/components/tui/PaymentInfo.tsx
@@ -2,7 +2,6 @@ import useSWRImmutable from "swr/immutable";
 import { CustomerExists } from "types";
 import { useCustomer } from "~utils/hooks/useCustomer";
 import { useApiClient } from "~utils/hooks/useApiClient";
-import { useRouter } from "next/router";
 
 const PaymentInfo = () => {
   const { customer, update, isValidating } = useCustomer();
@@ -13,7 +12,7 @@ const PaymentInfo = () => {
   const client = useApiClient();
 
   const getPortal = () => {
-    const result = client.post("stripe/createPortalSession", {
+    const result = client.post("customer/createPortalSession", {
       body: JSON.stringify({
         url: window.location.href,
       }),
@@ -21,7 +20,8 @@ const PaymentInfo = () => {
 
     result.then((res) => {
       res.json().then((data) => {
-        window.open(data.url, "_blank");
+        // window.open(data.url);
+        window.location.href = data.url;
         mutateExists();
       });
     });
@@ -38,19 +38,17 @@ const PaymentInfo = () => {
         </p>
       </div>
       <div className="mt-5 md:mt-0 md:col-span-2 flex flex-col align-middle">
-        {customerState?.exists ? undefined : (
-          <div className="self-center w-max flex flex-col justify-around">
-            <p className="text-sm text-center text-gray-500 italic">
-              You are not a registered customer.
-            </p>
-            <button
-              onClick={getPortal}
-              className="mt-2 inline-flex justify-center py-1 px-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-brand-primary hover:bg-brand-accent1h focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-accent2h"
-            >
-              Register a payment method
-            </button>
-          </div>
-        )}
+        <div className="self-center w-max flex flex-col justify-around">
+          <p className="text-sm text-center text-gray-500">
+            Edit your Billing Information on Stripe
+          </p>
+          <button
+            onClick={getPortal}
+            className="mt-2 inline-flex justify-center py-1 px-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-brand-primary hover:bg-brand-accent1h focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-accent2h"
+          >
+            Open Secure Page
+          </button>
+        </div>
       </div>
     </div>
   );
