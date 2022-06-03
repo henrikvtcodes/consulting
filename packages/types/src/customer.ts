6729169553fbba1df216974b155fa328811d650d
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-06-02 21:43:18 -0400

    types: new customer type

--- packages/types/src/customer.ts
+++ packages/types/src/customer.ts
@@ -1,10 +1,5 @@
+import { Stripe } from "stripe";
 import { AddressOpt } from "./address";
-
-export interface Customer extends AddressOpt {
-  firstName: string;
-  lastName: string;
-  separateAddr: boolean;
-}
 
 export interface CustomerDetailsForm extends AddressOpt {
   firstName?: string;
@@ -18,4 +13,9 @@ export interface CustomerExists {
         stripe: boolean;
       }
     | false;
+}
+
+export interface Customer {
+  customer: Stripe.Response<Stripe.Customer | Stripe.DeletedCustomer>;
+  paymentMethods: Stripe.PaymentMethod[];
 }
