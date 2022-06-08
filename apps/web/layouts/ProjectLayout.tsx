a8dbef9261654129fafc7c4ae7470606f6ee5b38
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-06-08 16:28:03 +0000

    web: fix back button arrow

--- apps/web/layouts/ProjectLayout.tsx
+++ apps/web/layouts/ProjectLayout.tsx
@@ -26,9 +26,10 @@ const ProjectLayout = ({ children }: { children?: React.ReactNode }) => {
       <button
         type="button"
         onClick={() => router.back()}
-        className="inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        className="inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-primary"
       >
-        <ArrowSmLeftIcon /> Back
+        <ArrowSmLeftIcon className="text-gray-700 h-auto w-4" />
+        Back
       </button>
       <div className="py-5 border-b border-gray-200">
         <h3 className="text-lg leading-6 font-medium text-gray-900 ">
