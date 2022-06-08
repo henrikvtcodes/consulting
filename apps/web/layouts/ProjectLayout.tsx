54589758dd17aaac6d66846f2e0c34ce6a5bb289
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-06-08 16:06:50 +0000

    web: add error handling

--- apps/web/layouts/ProjectLayout.tsx
+++ apps/web/layouts/ProjectLayout.tsx
@@ -1,17 +1,36 @@
+import { ArrowSmLeftIcon } from "@heroicons/react/outline";
+import { HTTPError } from "ky";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import { ProjectData } from "types";
 import { ProjectStatusIndicator } from "~components/tui/ProjectCard";
+import { ErrorComponent } from "../pages/404";
 
 const ProjectLayout = ({ children }: { children?: React.ReactNode }) => {
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, error } = useSWR<ProjectData>(`project/${id}`);
+  const { data, error } = useSWR<ProjectData, HTTPError>(`project/${id}`);
+
+  if (error && error.response.status === 404) {
+    return (
+      <div className="my-auto">
+        {" "}
+        <ErrorComponent url="/client/projects" label="to projects" />
+      </div>
+    );
+  }
 
   return (
     <div>
-      <div className="pb-5 border-b border-gray-200">
+      <button
+        type="button"
+        onClick={() => router.back()}
+        className="inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >
+        <ArrowSmLeftIcon /> Back
+      </button>
+      <div className="py-5 border-b border-gray-200">
         <h3 className="text-lg leading-6 font-medium text-gray-900 ">
           {data?.name ? data.name : "Loading..."}
         </h3>
