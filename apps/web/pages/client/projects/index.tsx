8efb19c84a93db4e96a4194faf6c2cb7cd44ec22
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-06-09 18:49:24 +0000

    web: project page error handling

--- apps/web/pages/client/projects/index.tsx
+++ apps/web/pages/client/projects/index.tsx
@@ -2,13 +2,29 @@ import type { NextPage } from "next";
 import { Project } from "@prisma/client";
 import useSWR from "swr";
 import { useState } from "react";
+import { HTTPError } from "ky";
 
 import { ClientDashLayout } from "layouts/ClientDash";
 import ProjectCard from "~components/tui/ProjectCard";
 import { RequestProject } from "~components/forms/RequestProject";
 
 const Page: NextPage = () => {
-  const { data, error } = useSWR<Project[]>(`project`);
+  const { data, error } = useSWR<Project[]>(`project`, {
+    onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+      const err = error as HTTPError;
+      if (err.response.status === 400) {
+        return;
+      }
+    },
+  });
+
+  let message: string | null = null;
+
+  const err = error as HTTPError;
+  if (err?.response.status === 400) {
+    message =
+      "Please go to your Account page and register your billing information.";
+  }
 
   const [openRequest, setOpenRequest] = useState(false);
   return (
@@ -19,17 +35,22 @@ const Page: NextPage = () => {
         <span>
           <button
             type="button"
+            disabled={message !== null}
             onClick={() => setOpenRequest(true)}
             className="justify-self-end inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-brand-primary hover:bg-brand-accent1h focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-accent2"
           >
-            Request Project
+            {message ? "Unavailable" : "Request Project"}
           </button>
         </span>
       </div>
       <div className="flex flex-col space-y-2">
-        {data?.map((project) => {
-          return <ProjectCard project={project} key={project.id} />;
-        })}
+        {data?.length > 0 ? (
+          data?.map((project) => {
+            return <ProjectCard project={project} key={project.id} />;
+          })
+        ) : (
+          <div>{message ? message : "Request a project!"}</div>
+        )}
       </div>
     </ClientDashLayout>
   );
